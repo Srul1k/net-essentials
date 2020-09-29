15128fa78732461a8d5f267b0527632cd7f45151
Srul1k <srul1k@github.com>
2020-09-29 19:23:07 +0300

    created ConsoleCalculator
    Implemented application logic.

---
+++ ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            var buffer = Console.ReadLine().Split(' ');
+
+            if (buffer.Length == 3 &&
+                TryParseWithSpecifiedRange(buffer[0], out double x) &&
+                TryParseWithSpecifiedRange(buffer[1], out double y))
+            {
+                switch (buffer[2])
+                {
+                    case "A":
+                        Console.WriteLine(x + y);
+                        break;
+                    case "D":
+                        Console.WriteLine(x / y);
+                        break;
+                    case "M":
+                        Console.WriteLine(x * y);
+                        break;
+                    case "S":
+                        Console.WriteLine(x - y);
+                        break;
+                    default:
+                        Console.WriteLine("error");
+                        break;
+                }
+            }
+            else Console.WriteLine("error");
+        }
+
+        private static bool TryParseWithSpecifiedRange(string s, out double x)
+        {
+            bool result;
+
+            var buffer = s.Split('.');
+            if (buffer.Length > 1)
+                if (buffer[1].Length > 5)
+                {
+                    x = double.NaN;
+                    return false;
+                }
+            
+            result = double.TryParse(s, out x);
+            return result && x >= -10000 & x <= 10000;
+        }
+    }
+}

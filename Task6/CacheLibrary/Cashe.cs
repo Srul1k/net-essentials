e5a1beed0b96c6eb19a1d80ecefe67d3da9e9ecf
Srul1k <srul1k.post@ya.ru>
2020-10-12 13:26:00 +0300

    created task6
    - interface implemented
    - added usage examples

---
+++ Task6/CacheLibrary/Cashe.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CacheLibrary
+{
+    public class Cashe<TKey, TValue> : ICache<TKey, TValue> 
+        where TKey : struct 
+        where TValue : class
+    {
+        private struct CasheItem
+        {
+            public TKey Key { get; set; }
+            public TValue Value { get; set; }
+            public DateTime ExpiresOn { get; set; }
+
+            public CasheItem(TKey key, TValue value, DateTime expiresOn)
+            {
+                Key = key;
+                Value = value;
+                ExpiresOn = expiresOn;
+            }
+        }
+
+        private List<CasheItem> items;
+
+        public Cashe() { items = new List<CasheItem>(); }
+
+        public void AddOrUpdate(TKey key, TValue value, DateTime expiresOn)
+        {
+            foreach (var item in items)
+            {
+                if (item.Key.Equals(key))
+                {
+                    items.Remove(item);
+                }
+            }
+            
+            items.Add(new CasheItem(key, value, expiresOn));
+        }
+
+        public TValue Get(TKey key)
+        {
+            foreach (var item in items)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.ExpiresOn > DateTime.Now ? item.Value : null;
+                }
+            }
+
+            return null;
+        }
+
+        public bool Remove(TKey key)
+        {
+            foreach (var item in items)
+            {
+                if (item.Key.Equals(key))
+                {
+                    items.Remove(item);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Task6/CacheLibrary/ICashe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CacheLibrary
+{
+   public interface ICache<TKey, TValue>
+    where TKey : struct
+    where TValue : class
+    {
+        // Adds an item with specified key and value to the cache.
+        // Updates the item value and expiresOn date if the item with the key already exists in cache.
+        void AddOrUpdate(TKey key, TValue value, DateTime expiresOn);
+
+
+        // Returns the value associated with the specified key.
+        // Returns null if DateTime.Now less than expiresOn date.
+        // Returns null if there is no value associated with the specified key.
+        TValue Get(TKey key);
+
+        // Removes the value with the specified key from the cache.
+        // Returns true if the value with the specified key is removed.
+        // Returns false if there is no value associated with the specified key.
+        bool Remove(TKey key);
+    }
+}

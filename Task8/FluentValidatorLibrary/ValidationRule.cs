ee6108268d9e8a33d767024a774be76cbc868290
Srul1k <srul1k.post@ya.ru>
2020-10-23 13:22:46 +0300

    completed Task8
    - created FluentValidatorLibrary
    - added UsageExamples

---
+++ Task8/FluentValidatorLibrary/ValidationRule.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FluentValidatorLibrary
+{
+    public struct ValidationRule<T>
+    {
+        public Predicate<T> IsValid { get; set; }
+        public Func<T, string> ErrorMessage { get; set; }
+    }
+}

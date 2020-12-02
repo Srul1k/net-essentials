cb8ddf218e39c56cdd920cdf6cbb2bb9ecab37ae
Srul1k <srul1k.post@ya.ru>
2020-12-02 16:44:56 +0300

    Implemented Task10

---
+++ Task10/ExceptionsLibrary/WrongGenderException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExceptionsLibrary
+{
+    public class WrongGenderException : ArgumentException
+    {
+        public string Value { get; }
+        public WrongGenderException() { }
+        public WrongGenderException(string message) : base(message) { }
+        public WrongGenderException(string message, string value) : this(message)
+        {
+            Value = value;
+        }
+    }
+}

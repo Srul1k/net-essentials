85ef4f4b9aa2466577a0531f74c03ef5f4dfb369
Srul1k <kuch1kukan@keemail.me>
2021-01-23 21:25:06 +0300

    Add Task11

---
+++ Task11/ConsoleApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using TaskLibrary;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            //CreateTaskWithDelay();
+            //Create10TasksWithDelay();
+            //Create10TasksWithoutDelay();
+        }
+
+        static void CreateTaskWithDelay()
+        {
+            CancellationTokenSource cancelTokenSource = new CancellationTokenSource(2000);
+            CancellationToken token = cancelTokenSource.Token;
+
+            Task task = WorkWithTasks.CreateTask(token);
+            task.Wait();
+        }
+
+        static void Create10TasksWithDelay()
+        {
+            CancellationTokenSource cancelTokenSource = new CancellationTokenSource(2000);
+            CancellationToken token = cancelTokenSource.Token;
+
+            Task[] tasks = new Task[10];
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = WorkWithTasks.CreateTask(token);
+            }
+
+            Task.WaitAll(tasks);
+        }
+
+        static void Create10TasksWithoutDelay()
+        {
+            CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancelTokenSource.Token;
+
+            Task[] tasks = new Task[10];
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = WorkWithTasks.CreateTask(token);
+            }
+
+            Task.WaitAny(tasks);
+        }
+    }
+}
---
+++ Task11/TaskLibrary/WorkWithTasks.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskLibrary
+{
+    public static class WorkWithTasks
+    {
+        public static Task CreateTask(CancellationToken ct = default)
+        {
+            Random rnd = new Random();
+            int iterationsMax = rnd.Next(5, 30);
+
+            return Task.Run(() =>
+            {
+                for (int i = 0; i < iterationsMax; i++)
+                {
+                    if (ct.IsCancellationRequested)
+                    {
+                        return;
+                    }
+
+                    Console.WriteLine($"Thread:{Thread.CurrentThread.ManagedThreadId}. " +
+                        $"IterationsMax: {iterationsMax}. IterationCurrent: {i}");
+                    Thread.Sleep(rnd.Next(200, 300));
+                }
+            });
+        }
+    }
+}
